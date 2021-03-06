fce320acccdadaa271eb80bd409cfcf8c68f36f6
saadullah01 <saadullah1912@gmail.com>
2021-03-06 14:32:22 +0500

    script updated

--- auto-script.py
+++ auto-script.py
@@ -12,28 +12,42 @@ import pexpect, os, sys, time
 
 f = open(sys.argv[1], "r")
 
-arguments = f.readline()[:-1].split(",")
+parameters = f.readline()[:-1].split(",")
 
-timeout = int(arguments[0])
-commit_comment = arguments[1]
+# Parameters from file
+timeout = int(parameters[0])
+commit_comment = parameters[1]
+clone_branch = parameters[2]
+commit_branch = parameters[3]
 
-# Environment Variables
+# Variables
 username = os.environ["username"]
 password = os.environ["access_token"]
-cmd_dir = "/Educative/" + os.environ["repository_name"]
+repo_name = os.environ["repository_name"]
+cmd_dir = "/Educative/" + repo_name
+
+# Commit Command
 commit_cmd = "git commit -m '" + commit_comment + "'"
+
+# Push Command
 push_cmd = "git push"
+if commit_branch != "none":
+    push_cmd = "git push --set-upstream origin " + commit_branch
 
-if len(arguments) == 3:
-    branch_name = arguments[0]
-    push_cmd = "git push --set-upstream origin " + branch_name
-
-commands = f.readlines()
+# Clone Command
+clone_cmd = "git clone https://github.com/"+username+"/"+repo_name+".git"
+if clone_branch != "none":
+    clone_cmd = "git clone -b " + clone_branch + " https://github.com/"+username+"/"+repo_name+".git"
 
 print("Pushing Code to GitHub:")
+pexpect.run(clone_cmd, cwd="/Educative")
+
+# Pre-push commands
+commands = [i[:-1].split(",") for i in f.readlines()]
 for cmd in commands:
-    cd = cmd[:-1].split(",")
-    pexpect.run(cd[0], cwd=cd[1])
+    pexpect.run(cmd[0], cwd=cmd[1])
+
+# Add, commit and push
 pexpect.run("git add .", cwd=cmd_dir)
 pexpect.run(commit_cmd, cwd=cmd_dir)
 ch = pexpect.spawn(push_cmd, cwd=cmd_dir)
