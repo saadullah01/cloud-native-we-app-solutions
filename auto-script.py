0eee5e96184c9d04a8f2e9c50462e91b9feb102c
saadullah01 <saadullah1912@gmail.com>
2021-03-06 14:09:20 +0500

    script updated

--- auto-script.py
+++ auto-script.py
@@ -10,20 +10,30 @@
 
 import pexpect, os, sys, time
 
-timeout = int(sys.argv[1])
-commit_comment = sys.argv[2]
-branch_name = sys.argv[3]
+f = open(sys.argv[1], "r")
 
+arguments = f.readline()[:-1].split(",")
+
+timeout = int(arguments[0])
+commit_comment = arguments[1]
+
+# Environment Variables
 username = os.environ["username"]
 password = os.environ["access_token"]
 cmd_dir = "/Educative/" + os.environ["repository_name"]
 commit_cmd = "git commit -m '" + commit_comment + "'"
 push_cmd = "git push"
 
-if branch_name:
+if len(arguments) == 3:
+    branch_name = arguments[0]
     push_cmd = "git push --set-upstream origin " + branch_name
 
+commands = f.readlines()
+
 print("Pushing Code to GitHub:")
+for cmd in commands:
+    cd = cmd[:-1].split(",")
+    pexpect.run(cd[0], cwd=cd[1])
 pexpect.run("git add .", cwd=cmd_dir)
 pexpect.run(commit_cmd, cwd=cmd_dir)
 ch = pexpect.spawn(push_cmd, cwd=cmd_dir)
