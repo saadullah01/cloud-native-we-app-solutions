c9e1bafbbf1c4ffed2f62b283c5fdfb0ad1e5fed
Saad Ullah <saadullah1912@gmail.com>
2021-03-13 17:18:54 +0500

    Update script.py

--- script.py
+++ script.py
@@ -7,22 +7,34 @@
 
 import pexpect, os, sys, time
 
-
-# Parameters
-push_timeout = 10
-
-# Variables
+# Environment Variables
 username = os.environ["username"]
 password = os.environ["access_token"]
 repo_name = os.environ["repository_name"]
+
+# 1) Creating repo
+print("Creating GitHub Project '" + repo_name + "':")
+create_repo = pexpect.spawn("curl -u '" + username + "' https://api.github.com/user/repos -d '{\"name\":\"" + repo_name + "\"}'")
+create_repo.expect("Enter host password for user .*:")
+create_repo.sendline(password)
+time.sleep(5)
+print("Done")
+
+# 2) Pushing monorepo template to GitHub
+print("Pushing monorepo template to GitHub")
+pexpect.run("mv /Educative/my_cloud_native_web_app " + repo_name) # Changing name
+
 cmd_dir = "/Educative/" + repo_name + "/"
 
+pexpect.run("git init", cwd=cmd_dir)
 pexpect.run("git add .", cwd=cmd_dir)
-pexpect.run("git commit -m 'message'", cwd=cmd_dir)
-ch = pexpect.spawn('git push', cwd=cmd_dir)
+pexpect.run("git commit -m 'my first commit'", cwd=cmd_dir)
+pexpect.run("git branch -M main", cwd=cmd_dir)
+pexpect.run("git remote add origin https://github.com/" + username + "/" + repo_name + ".git", cwd=cmd_dir)
+ch = pexpect.spawn("git push -u origin main", cwd=cmd_dir)
 ch.expect('Username for .*:')
 ch.sendline(username)
 ch.expect('Password for .*:')
 ch.sendline(password)
-time.sleep(push_timeout)
+time.sleep(5)
 print("Done")
