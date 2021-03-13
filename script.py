ec9377f5b3badc7f9909a0059402c4dd5ed528fd
saadullah01 <saadullah1912@gmail.com>
2021-03-13 16:28:42 +0500

    updated script

--- script.py
+++ script.py
@@ -7,15 +7,9 @@
 
 import pexpect, os, sys, time
 
-f = open(sys.argv[1], "r")
 
-parameters = f.readline()[:-1].split(",")
-
-# Parameters from file
-push_timeout = int(parameters[0])
-commit_comment = parameters[1]
-clone_branch = parameters[2]
-commit_branch = parameters[3]
+# Parameters
+push_timeout = 10
 
 # Variables
 username = os.environ["username"]
@@ -23,26 +17,9 @@ password = os.environ["access_token"]
 repo_name = os.environ["repository_name"]
 cmd_dir = "/Educative/" + repo_name + "/"
 
-# Commit Command
-commit_cmd = "git commit -m '" + commit_comment + "'"
-
-# Push Command
-push_cmd = "git push"
-if commit_branch != "none":
-    push_cmd = "git push --set-upstream origin " + commit_branch
-
-# Clone Command
-clone_cmd = "git clone https://github.com/"+username+"/"+repo_name+".git"
-if clone_branch != "none":
-    clone_cmd = "git clone -b " + clone_branch + " https://github.com/"+username+"/"+repo_name+".git"
-
-print("Pushing Code to GitHub:")
-pexpect.run(clone_cmd, cwd="/Educative")
-
-# Add, commit and push
 pexpect.run("git add .", cwd=cmd_dir)
-pexpect.run(commit_cmd, cwd=cmd_dir)
-ch = pexpect.spawn(push_cmd, cwd=cmd_dir)
+pexpect.run("git commit -m 'message'", cwd=cmd_dir)
+ch = pexpect.spawn('git push', cwd=cmd_dir)
 ch.expect('Username for .*:')
 ch.sendline(username)
 ch.expect('Password for .*:')
