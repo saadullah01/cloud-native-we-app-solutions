8016bffd14bd7d2e869c201e9e6defe3dc7b6d5e
saadullah01 <saadullah1912@gmail.com>
2021-03-15 04:26:54 +0500

    ch-2/lesson-5

--- script.py
+++ script.py
@@ -7,22 +7,30 @@
 
 import pexpect, os, sys, time
 
-
-# Parameters
-push_timeout = 10
-
 # Variables
 username = os.environ["username"]
 password = os.environ["access_token"]
 repo_name = os.environ["repository_name"]
+author = os.environ["author"]
+
+print("Pushing code to GitHub Repo '"+repo_name+"'...")
+# Clone user repo
+pexpect.run(
+    "git clone https://github.com/"+username+"/"+repo_name+".git",
+    cwd="/Educative/")
+
+# Running solution commands
 cmd_dir = "/Educative/" + repo_name + "/"
 
-pexpect.run("git add .", cwd=cmd_dir)
-pexpect.run("git commit -m 'message'", cwd=cmd_dir)
-ch = pexpect.spawn('git push', cwd=cmd_dir)
+pexpect.run("git switch -c add-the-web-application", cwd=cmd_dir)
+pexpect.run("mkdir services", cwd=cmd_dir)
+pexpect.run("npx degit \"sveltejs/sapper-template#rollup\" web", cwd=cmd_dir+"services/")
+pexpect.run("git add -A services/", cwd=cmd_dir)
+pexpect.run("git commit -m \"Add the web service, based on the Sapper template.\"", cwd=cmd_dir)
+ch = pexpect.spawn("git push --set-upstream origin add-the-web-application", cwd=cmd_dir)
 ch.expect('Username for .*:')
 ch.sendline(username)
 ch.expect('Password for .*:')
 ch.sendline(password)
-time.sleep(push_timeout)
+time.sleep(5)
 print("Done")
