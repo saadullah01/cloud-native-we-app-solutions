c81fcf96de21424685704299584fc2076d157e89
saadullah01 <saadullah1912@gmail.com>
2021-03-19 20:38:05 +0500

    Script updated.

--- script.py
+++ script.py
@@ -7,15 +7,26 @@
 
 import pexpect, os, sys, time
 
-
-# Parameters
-push_timeout = 10
-
 # Variables
 username = os.environ["username"]
 password = os.environ["access_token"]
 repo_name = os.environ["repository_name"]
+
+# Clone user's repo
+pexpect.run(
+    "git clone -b enable-firebase-hosting https://github.com/"+username+"/"+repo_name+".git",
+    cwd="/Educative/")
+
 cmd_dir = "/Educative/" + repo_name + "/"
+
+# Handling .firebaserc
+data = {
+  "projects": {
+    "default": firebase_proj
+  }
+}
+with open(cmd_dir+"services/web/firebase/.firebaserc", "w") as jsonFile:
+    json.dump(data, jsonFile, indent=2)
 
 pexpect.run("git add .", cwd=cmd_dir)
 pexpect.run("git commit -m 'message'", cwd=cmd_dir)
@@ -24,5 +35,5 @@ ch.expect('Username for .*:')
 ch.sendline(username)
 ch.expect('Password for .*:')
 ch.sendline(password)
-time.sleep(push_timeout)
+time.sleep(5)
 print("Done")
