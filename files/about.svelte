a93a790fd5a0cf65d43e131fe2601c5677889950
saadullah01 <saadullah1912@gmail.com>
2021-03-22 14:33:48 +0500

    ch-6/lesson-3

---
+++ files/about.svelte
@@ -0,0 +1,16 @@
+<script>
+  import FeatureToggle from "../components/feature-toggle.svelte";
+  import { FEATURE_TOGGLES } from "../feature-toggles";
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
+  <p>Some people see this, others don't!</p>
+</FeatureToggle>
