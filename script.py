8822534a591da56ac4c5b6b9c380d4d2db0ea64f
saadullah01 <saadullah1912@gmail.com>
2021-03-24 08:03:31 +0500

    ch-8/lesson-4

--- script.py
+++ script.py
@@ -16,10 +16,11 @@ firebase_proj = os.environ["firebase_proj_name"]
 print("Pushing code to GitHub repo '"+repo_name+"'...")
 # Clone user's repo
 pexpect.run(
-    "git clone -b enable-firebase-hosting https://github.com/"+username+"/"+repo_name+".git",
+    "git clone -b add-tests https://github.com/"+username+"/"+repo_name+".git",
     cwd="/Educative/")
 
 cmd_dir = "/Educative/" + repo_name + "/"
+sol_dir = "/Educative/cnwa-solution-files/files/"
 
 # Handling .firebaserc
 data = {
@@ -30,6 +31,11 @@ data = {
 with open(cmd_dir+"services/web/firebase/.firebaserc", "w") as jsonFile:
     json.dump(data, jsonFile, indent=2)
 
+pexpect.run("cp -r "+sol_dir+"cypress "+cmd_dir+"services/web/")
+pexpect.run("cp "+sol_dir+"cypress.json "+cmd_dir+"services/web/")
+pexpect.run("cp "+sol_dir+"package-lock.json "+cmd_dir+"services/web/")
+pexpect.run("cp "+sol_dir+"package.json "+cmd_dir+"services/web/")
+pexpect.run("cp "+sol_dir+"rollup.config.js "+cmd_dir+"services/web/")
 pexpect.run("git add .", cwd=cmd_dir)
 pexpect.run("git commit -m 'message'", cwd=cmd_dir)
 ch = pexpect.spawn('git push', cwd=cmd_dir)
