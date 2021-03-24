60cdd2dcbcb313c60cf34cae79b9f5b3f0395b62
saadullah01 <saadullah1912@gmail.com>
2021-03-24 12:01:46 +0500

    ch-8/lesson-8

--- script.py
+++ script.py
@@ -16,10 +16,11 @@ firebase_proj = os.environ["firebase_proj_name"]
 print("Pushing code to GitHub repo '"+repo_name+"'...")
 # Clone user's repo
 pexpect.run(
-    "git clone -b enable-firebase-hosting https://github.com/"+username+"/"+repo_name+".git",
+    "git clone -b add-tests https://github.com/"+username+"/"+repo_name+".git",
     cwd="/Educative/")
 
 cmd_dir = "/Educative/" + repo_name + "/"
+sol_dir = "/Educative/cnwa-solution-files/files/"
 
 # Handling .firebaserc
 data = {
@@ -30,8 +31,10 @@ data = {
 with open(cmd_dir+"services/web/firebase/.firebaserc", "w") as jsonFile:
     json.dump(data, jsonFile, indent=2)
 
+pexpect.run("rm -rf .github/workflows/services-web-deploy.yml")
+pexpect.run("cp "+sol_dir+"services-web.yml "+cmd_dir+".github/workflows/")
 pexpect.run("git add .", cwd=cmd_dir)
-pexpect.run("git commit -m 'message'", cwd=cmd_dir)
+pexpect.run("git commit -m 'Enable Tests in the Continuous Integration Pipeline'", cwd=cmd_dir)
 ch = pexpect.spawn('git push', cwd=cmd_dir)
 ch.expect('Username for .*:')
 ch.sendline(username)
