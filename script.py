ee439c6b2efeb44a72b68e68df1f1a1c32df92c4
saadullah01 <saadullah1912@gmail.com>
2021-03-24 11:15:55 +0500

    ch-8/lesson-7

--- script.py
+++ script.py
@@ -12,14 +12,16 @@ username = os.environ["username"]
 password = os.environ["access_token"]
 repo_name = os.environ["repository_name"]
 firebase_proj = os.environ["firebase_proj_name"]
+cypress_projectID = os.environ["cypress_projectID"]
 
 print("Pushing code to GitHub repo '"+repo_name+"'...")
 # Clone user's repo
 pexpect.run(
-    "git clone -b enable-firebase-hosting https://github.com/"+username+"/"+repo_name+".git",
+    "git clone -b add-tests https://github.com/"+username+"/"+repo_name+".git",
     cwd="/Educative/")
 
 cmd_dir = "/Educative/" + repo_name + "/"
+sol_dir = "/Educative/cnwa-solution-files/files/"
 
 # Handling .firebaserc
 data = {
@@ -28,10 +30,20 @@ data = {
   }
 }
 with open(cmd_dir+"services/web/firebase/.firebaserc", "w") as jsonFile:
-    json.dump(data, jsonFile, indent=2)
+  json.dump(data, jsonFile, indent=2)
 
+data2 = {
+  "baseUrl": "http://localhost:3000/",
+  "componentFolder": "cypress/components",
+  "experimentalComponentTesting": True,
+  "projectId": cypress_projectID
+}
+with open(cmd_dir+"services/web/cypress.json", "w") as jsonFile:
+  json.dump(data2, jsonFile, indent=2)
+
+pexpect.run("cp "+sol_dir+"package.json "+cmd_dir+"services/web/")
 pexpect.run("git add .", cwd=cmd_dir)
-pexpect.run("git commit -m 'message'", cwd=cmd_dir)
+pexpect.run("git commit -m 'Cypress Dashboard'", cwd=cmd_dir)
 ch = pexpect.spawn('git push', cwd=cmd_dir)
 ch.expect('Username for .*:')
 ch.sendline(username)
