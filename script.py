116a120271aa1103316c5714dc2d2fea8886ced9
saadullah01 <saadullah1912@gmail.com>
2021-03-31 00:58:34 +0500

    update

--- script.py
+++ script.py
@@ -18,42 +18,18 @@ dev_api_key = os.environ["dev_api_key"]
 print("Pushing code to GitHub repo '"+repo_name+"'...")
 # Clone user's repo
 pexpect.run(
-    "git clone -b enable-firebase-hosting https://github.com/"+username+"/"+repo_name+".git",
+    "git clone -b event-based-triggers https://github.com/"+username+"/"+repo_name+".git",
     cwd="/Educative/")
 
 cmd_dir = "/Educative/" + repo_name + "/"
 sol_dir = "/Educative/cnwa-solution-files/files/"
 
-# Handling .firebaserc
-data = {
-  "projects": {
-    "default": firebase_proj
-  }
-}
-with open(cmd_dir+"services/web/firebase/.firebaserc", "w") as jsonFile:
-    json.dump(data, jsonFile, indent=2)
-
-# Handling cypress.json
-data2 = {
-  "baseUrl": "http://localhost:3000/",
-  "componentFolder": "cypress/components",
-  "experimentalComponentTesting": True,
-  "projectId": cypress_projectID
-}
-with open(cmd_dir+"services/web/cypress.json", "w") as jsonFile:
-  json.dump(data2, jsonFile, indent=2)
-
-# Handling cypress.json
-data3 = {
-  "crossposttodevto": {
-    "apikey": dev_api_key
-  }
-}
-with open(cmd_dir+"services/web/firebase/functions/.runtimeconfig.json", "w") as jsonFile:
-  json.dump(data3, jsonFile, indent=2)
-
+pexpect.run("cp "+sol_dir+"package.json "+cmd_dir+"services/web/firebase/functions/")
+pexpect.run("cp "+sol_dir+"package-lock.json "+cmd_dir+"services/web/firebase/functions/")
+pexpect.run("cp "+sol_dir+".gitignore "+cmd_dir+"services/web/firebase/functions/")
+pexpect.run("cp "+sol_dir+"index.test.js "+cmd_dir+"services/web/firebase/functions/src/firestore/posts/on-create/cross-post-to-devto")
 pexpect.run("git add .", cwd=cmd_dir)
-pexpect.run("git commit -m 'message'", cwd=cmd_dir)
+pexpect.run("git commit -m 'Write a test for the service'", cwd=cmd_dir)
 ch = pexpect.spawn('git push', cwd=cmd_dir)
 ch.expect('Username for .*:')
 ch.sendline(username)
